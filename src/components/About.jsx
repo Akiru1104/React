06c1f51c8f761f34569a78250597a83eb0470cce
25LP5290 25LP5290 <zulaa.1104@gmail.com>
2025-12-03 17:28:33 +0800

    shine

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <div>About me</div>
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>Curious about me? Here you have it:</div>
+        <div>
+          I'm a designer turned full stack developer, passionate about React.js
+          and Node.js. I excel in blending technical and visual aspects to craft
+          exceptional digital products, prioritizing user experience, precise
+          design, and optimized code.
+        </div>
+        <div>
+          Since starting my web development journey in 2015, I've embraced
+          challenges and kept up with the latest tech trends. Now in my early
+          thirties, seven years in, I'm building cutting-edge web apps using
+          Next.js, TypeScript, Nestjs, Tailwindcss, Supabase, and more.
+        </div>
+        <div>
+          With a progressive mindset, I enjoy the entire product development
+          process, from ideation to execution. Off duty, you'll find me on
+          Twitter, tracking startup journeys, or unwinding. Follow me for tech
+          insights and public project updates on Twitter or GitHub.
+        </div>
+        <div>Finally, some quick bits about me.</div>
+        <div>Check List</div>
+        <div>
+          One last thing, I'm available for freelance work, so feel free to
+          reach out and say hello! I promise I don't bite 😉
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,32 +2,43 @@ import React from "react";
 
 export const Info = () => {
   return (
-    <div>
-      <div>
-        <div>Hi,I'm Zulaa</div>
-        <div>
+    <div className="flex">
+      <div className="py-24 px-20  flex gap-12 flex-col">
+        <div className="text-6xl font-bold leading-16">Hi,I'm Zulaa 👋</div>
+        <div className="text-base font-normal">
           I specialize in full stack development, particularly with React.js and
           Node.js. My main goal is to create exceptional digital experiences
           that are fast, visually appealing, and accessible to everyone. With
           over 7 years of experience in web development, I continue to find joy
           in crafting innovative solutions and designs.
         </div>
-        <div>
-          <img src="" alt="" />
-          <div>Ulaanbaatar, Mongolia</div>
+        <div className=" flex  flex-col gap-2">
+          <div className="flex">
+            <img src="location.png" alt="" />
+            <div className="font-normal text-base leading-6">
+              Ulaanbaatar, Mongolia
+            </div>
+          </div>
+          <div className="flex">
+            <img src="green.png" alt="" />
+            <div className="font-normal text-base leading-6">
+              Available for new projects
+            </div>
+          </div>
         </div>
-        <div>
-          <img src="" alt="" />
-          <div>Available for new projects</div>
-        </div>
-        <div>
-          <img src="Icon.png" alt="" />
-          <img src="cat.png" alt="" />
-          <img src="" alt="" />
+
+        <div className="flex gap-1">
+          <img src="caticon.png" alt="" />
+          <img src="birdicon.png" alt="" />
+          <img src="3rdicon.png" alt="" />
         </div>
       </div>
       <div>
         <img src="" alt="" />
+      </div>
+      <div className="relative...">
+        <img className="border-8 border-[#030712]" src="Ariunzul.png" alt="" />
+        <img className="absolute " src="Background.png" alt="" />
       </div>
     </div>
   );
